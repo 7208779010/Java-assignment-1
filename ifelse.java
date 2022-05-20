e2efb7eeae93ab17f70ae3c5a89b38d54c485202
ANPC1489Sanskriti <92329337+7208779010@users.noreply.github.com>
2022-05-20 12:06:53 +0530

    Create ifelse.java

---
+++ ifelse.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class If_else {
+	public static void main(String[] args) {
+		
+		int num;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter your Number");
+		num=sc.nextInt();
+		if(num %5==0)
+		{
+			System.out.println("Number is Even");
+			
+		}
+		else
+		{
+			System.out.println("Number is Odd");
+		}
+	}
+
+}
+© 2022 GitHub, I
